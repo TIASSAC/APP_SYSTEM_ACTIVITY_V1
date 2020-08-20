66ab4d9a2e9145a60b43a84e781edc6046e045b1
GuerreroRJ <30504886+GuerreroRJ@users.noreply.github.com>
2020-08-20 11:54:53 -0500

    interfaces

--- app/src/main/java/com/example/appgcm/Listeners/LoginListener.java
+++ app/src/main/java/com/example/appgcm/Listeners/LoginListener.java
@@ -3,4 +3,6 @@ package com.example.appgcm.Listeners;
 public interface LoginListener {
 
     void goToMain();
+
+    void loadService();
 }
--- app/src/main/java/com/example/appgcm/Listeners/MainListener.java
+++ app/src/main/java/com/example/appgcm/Listeners/MainListener.java
@@ -1,7 +1,14 @@
 package com.example.appgcm.Listeners;
 
+import android.app.ProgressDialog;
+
 public interface MainListener {
-    void goToMain2();
     void goToMainFragment();
     void goToButtonFragment();
+    void CloseSession();
+
+    void showProgressDialog();
+    void dismissProgressDialog();
+    void updateActionBar(String texto);
+
 }
